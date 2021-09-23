d425f18a66d5cf374e176d5a592656d065219a4d
vassdeniss <vassdeniss@gmail.com>
2021-09-23 12:58:22 +0300

    Committed everything up to and including: Basics - More Exercises

---
+++ MoreExercises/ME04.ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ME04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] reversal = input.ToCharArray();
+            Array.Reverse(reversal);
+
+            Console.WriteLine(reversal);
+        }
+    }
+}
