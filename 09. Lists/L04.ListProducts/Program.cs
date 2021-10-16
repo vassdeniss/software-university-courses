8356b183ce5ceabff40d9573c2ac299274f97928
vassdeniss <vassdeniss@gmail.com>
2021-10-16 17:51:31 +0300

    Committed everything up to and including: Lists - Lab

---
+++ 09. Lists/L04.ListProducts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L04.ListProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productQuantity = int.Parse(Console.ReadLine());
+            List<string> productList = new List<string>();
+
+            for (int i = 0; i < productQuantity; i++)
+                productList.Add(Console.ReadLine());
+
+            productList.Sort();
+
+            for (int i = 0; i < productList.Count; i++)
+                Console.WriteLine($"{i + 1}.{productList[i]}");
+        }
+    }
+}
---
+++ 09. Lists/L05.RemveNegativesReverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L05.RemveNegativesReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers.RemoveAll(n => n < 0);
+            numbers.Reverse();
+
+            if (numbers.Count == 0)
+                Console.WriteLine("empty");
+            else
+                Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
