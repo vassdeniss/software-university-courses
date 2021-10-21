8f61e8c6ca358c85a0fc4805bb222b73b0ee3fc5
vassdeniss <vassdeniss@gmail.com>
2021-10-21 21:01:29 +0300

    Committed everything up to and including: Lists - More Exercises

---
+++ MoreExercises/ME01.Messaging/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ME01.Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = Console.ReadLine()
+                .Split().Select(int.Parse).ToList();
+            List<char> messageList = new List<char>();
+            messageList.AddRange(Console.ReadLine());
+
+            string result = string.Empty;
+            foreach (int n in numList)
+            {
+                int sum = 0;
+                int number = n;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+
+                try
+                {
+                    result += messageList[sum];
+                    messageList.RemoveAt(sum);
+                }
+                catch (Exception)
+                {
+                    int toCount = sum - messageList.Count;
+                    result += messageList[0 + toCount];
+                    messageList.RemoveAt(0 + toCount);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MoreExercises/ME02.CarRace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ME02.CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> times = Console.ReadLine()
+                .Split().Select(int.Parse).ToList();
+            double racerOneTime = 0;
+            double racerTwoTime = 0;
+
+            int middleIndex = (times.Count - 1) / 2;
+            for (int i = 0; i < middleIndex; i++)
+            {
+                if (times[i] == 0) racerOneTime *= 0.8;
+                racerOneTime += times[i];
+            }
+
+            for (int i = times.Count - 1; i > middleIndex; i--)
+            {
+                if (times[i] == 0) racerTwoTime *= 0.8;
+                racerTwoTime += times[i];
+            }
+
+            string winner = racerOneTime <= racerTwoTime ? "left" : "right";
+            double winnerTime = racerOneTime <= racerTwoTime ? racerOneTime : racerTwoTime;
+            Console.WriteLine($"The winner is {winner} with total time: {winnerTime:0.#####}");
+        }
+    }
+}
