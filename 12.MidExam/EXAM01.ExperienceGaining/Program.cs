d37161325e9b476cf11276f5041f3ac5ca16fe00
vassdeniss <vassdeniss@gmail.com>
2021-10-24 11:58:26 +0300

    Committed everything up to and including: Mid Exam

---
+++ 12.MidExam/EXAM01.ExperienceGaining/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EXAM01.ExperienceGaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int expNeeded = int.Parse(Console.ReadLine());
+            int battlesQty = int.Parse(Console.ReadLine());
+
+            int battleCount = 0;
+            double totalExp = 0;
+            for (int i = 1; i <= battlesQty; i++)
+            {
+                battleCount++;
+                int gainedExp = int.Parse(Console.ReadLine());
+                totalExp += gainedExp;
+                if (i % 3 == 0) totalExp += gainedExp * 0.15;
+                if (i % 5 == 0) totalExp -= gainedExp * 0.10;
+                if (i % 15 == 0) totalExp += gainedExp * 0.05;
+                if (totalExp >= expNeeded) break;
+            }
+
+            if (totalExp >= expNeeded)
+                Console.WriteLine($"Player successfully collected his needed experience for {battleCount} battles.");
+            else
+                Console.WriteLine($"Player was not able to collect the needed experience, {expNeeded - totalExp:f2} more needed.");
+        }
+    }
+}
---
+++ 12.MidExam/EXAM03.TheAngryCat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXAM03.TheAngryCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> priceRatings = Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToList();
+            int entryPoint = int.Parse(Console.ReadLine());
+            string itemType = Console.ReadLine();
+
+            List<int> leftSide = new List<int>();
+            List<int> rightSide = new List<int>();
+            for (int i = 0; i < entryPoint; i++)
+                leftSide.Add(priceRatings[i]);
+            for (int i = entryPoint + 1; i < priceRatings.Count; i++)
+                rightSide.Add(priceRatings[i]);
+
+            int leftSum = 0;
+            int rightSum = 0;
+            int comparator = priceRatings[entryPoint];
+            foreach (int n in leftSide)
+            {
+                if (itemType == "cheap" && n < comparator)
+                    leftSum += n;
+                else if (itemType == "expensive" && n >= comparator)
+                    leftSum += n;
+            }
+            foreach (int n in rightSide)
+            {
+                if (itemType == "cheap" && n < comparator)
+                    rightSum += n;
+                else if (itemType == "expensive" && n >= comparator)
+                    rightSum += n;
+            }
+
+            if (leftSum >= rightSum)
+                Console.WriteLine($"Left - {leftSum}");
+            else
+                Console.WriteLine($"Right - {rightSum}");
+        }
+    }
+}
