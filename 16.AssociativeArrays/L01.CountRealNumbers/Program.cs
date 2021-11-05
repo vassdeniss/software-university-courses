0794fec95c0029d489b1c7c007b9e7be476b6a79
vassdeniss <vassdeniss@gmail.com>
2021-11-05 14:21:09 +0200

    Committed everything up to and including: Associative Arrays - Lab

---
+++ 16.AssociativeArrays/L01.CountRealNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split()
+                .Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> count = new SortedDictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (count.ContainsKey(num)) count[num]++;
+                else count.Add(num, 1);
+            }
+
+            foreach (KeyValuePair<int, int> pair in count)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ 16.AssociativeArrays/L04.LargestThreeNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace L04.LargestThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split()
+                .Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
