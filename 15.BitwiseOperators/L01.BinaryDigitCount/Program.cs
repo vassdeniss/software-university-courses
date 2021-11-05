6aa8ce70f13c37b23f24de5c03b5df65ad518650
vassdeniss <vassdeniss@gmail.com>
2021-11-05 11:17:35 +0200

    Committed everything up to and including: Bitwise Operators - Lab

---
+++ 15.BitwiseOperators/L01.BinaryDigitCount/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L01.BinaryDigitCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int digit = int.Parse(Console.ReadLine());
+
+            string binaryDigit = ConvertToBinary(num);
+
+            Console.WriteLine(FindDigits(binaryDigit, digit));
+        }
+
+        static int FindDigits(string binaryDigit, int digit)
+        {
+            int count = 0;
+            foreach (char c in binaryDigit)
+            {
+                double numC = char.GetNumericValue(c);
+                if (numC == digit) count++;
+            }
+
+            return count;
+        }
+
+        static string ConvertToBinary(int n)
+        {
+            string binaryNumber = string.Empty;
+
+            while (n > 0)
+            {
+                binaryNumber += n % 2;
+                n /= 2;
+            }
+
+            IEnumerable<char> binaryNumberArray = binaryNumber.ToCharArray().Reverse();
+
+            return new string(binaryNumberArray.ToArray());
+        }
+    }
+}
---
+++ 15.BitwiseOperators/L02.BitAtFirstPosition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace L02.BitAtFirstPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num >> 1) & 1);
+        }
+    }
+}
