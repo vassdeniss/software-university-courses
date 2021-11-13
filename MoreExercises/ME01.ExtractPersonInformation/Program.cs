daee87e3c0870d7f33c037c7b28c669ab288d3e9
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-11-13 18:13:22 +0200

    Committed everything up to and including: Text Processing - More Exercises

---
+++ MoreExercises/ME01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ME01.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesQty = int.Parse(Console.ReadLine());
+            for (int i = 0; i < linesQty; i++)
+            {
+                string input = Console.ReadLine();
+                int atSymbol = input.IndexOf('@');
+                int pipeSymbol = input.IndexOf('|');
+                int hashSymbol = input.IndexOf('#');
+                int starSymbol = input.IndexOf('*');
+
+                string name = input[(atSymbol + 1)..pipeSymbol];
+                string age = input[(hashSymbol + 1)..starSymbol];
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ MoreExercises/ME03.TreasureFinder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ME03.TreasureFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] decrypters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = Console.ReadLine();
+            while (input != "find")
+            {
+                Console.WriteLine(FindTreasure(DecryptMessage(input, decrypters)));
+                input = Console.ReadLine();
+            }
+        }
+
+        static string DecryptMessage(string encrypted, int[] decrypters)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int iterator = 0;
+            foreach (char c in encrypted)
+            {
+                sb.Append((char)(c - decrypters[iterator]));
+                iterator++;
+                if (iterator >= decrypters.Length) iterator = 0;
+            }
+
+            return sb.ToString();
+        }
+
+        static string FindTreasure(string decrypted)
+        {
+            int treasureStart = decrypted.IndexOf('&');
+            int treasureEnd = decrypted.IndexOf('&', treasureStart + 1);
+            int locationStart = decrypted.IndexOf('<');
+            int locationEnd = decrypted.IndexOf('>');
+
+            string treasure = decrypted[(treasureStart + 1)..treasureEnd];
+            string location = decrypted[(locationStart + 1)..locationEnd];
+
+            return $"Found {treasure} at {location}";
+        }
+    }
+}
