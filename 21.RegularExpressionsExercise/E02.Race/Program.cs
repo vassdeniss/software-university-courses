767d0183c8de036a0f79598e500e438eaacd727b
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-11-20 17:17:37 +0200

    Committed everything up to and including: Regular Expressions - Exercises

---
+++ 21.RegularExpressionsExercise/E02.Race/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace E02.Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> racers = 
+                Console.ReadLine().Split(", ").ToList();
+            Dictionary<string, int> racersScore = 
+                new Dictionary<string, int>();
+            Regex nameMatcher = new Regex(@"(?<letters>[A-Za-z])");
+            Regex digitsMatcher = new Regex(@"(?<digits>\d)");
+
+            string input = Console.ReadLine();
+            while (input != "end of race")
+            {
+                StringBuilder nameBuilder = new StringBuilder();
+                MatchCollection name = nameMatcher.Matches(input);
+                foreach (Match m in name)
+                    nameBuilder.Append(m.Value);
+
+                if (racers.Contains(nameBuilder.ToString()))
+                {
+                    StringBuilder scoreBuilder = new StringBuilder();
+                    MatchCollection digits = digitsMatcher.Matches(input);
+                    foreach (Match m in digits)
+                        scoreBuilder.Append(m.Value);
+
+                    int digitsNum = int.Parse(scoreBuilder.ToString());
+                    int score = 0;
+                    while (digitsNum > 0)
+                    {
+                        score += digitsNum % 10;
+                        digitsNum /= 10;
+                    }
+
+                    if (racersScore.ContainsKey(nameBuilder.ToString()))
+                        racersScore[nameBuilder.ToString()] += score;
+                    else
+                        racersScore.Add(nameBuilder.ToString(), score);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int iterator = 0;
+            string[] places = { "1st", "2nd", "3rd" };
+            foreach (KeyValuePair<string, int> pair in racersScore.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{places[iterator]} place: {pair.Key}");
+                iterator++;
+                if (iterator > 2) break;
+            }
+        }
+    }
+}
---
+++ 21.RegularExpressionsExercise/E06.ExtractEmails/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace E06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex matcher = new Regex(@"(^|\s)(?<user>[A-Za-z\d]+([_.-]*\w+)+)@(?<host>[A-Za-z]+([.-]\w+)+\b)");
+            MatchCollection matches = matcher.Matches(Console.ReadLine());
+            matches.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
