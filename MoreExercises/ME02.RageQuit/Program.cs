88aa4dba458079897958dd6d743d0b96230f6982
Denis Vasilev <vassdeniss@gmail.com>
2021-11-22 22:00:43 +0200

    Committed everything up to and including: Regular Expressions - More Exercises

---
+++ MoreExercises/ME02.RageQuit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ME02.RageQuit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex extractNumbers = new Regex(@"\d+");
+            Regex extractCharacters = new Regex(@"[^\d]+");
+            string input = Console.ReadLine();
+
+            string[] nums = extractNumbers.Matches(input).Cast<Match>().Select(m => m.Value).ToArray();
+            string[] chars = extractCharacters.Matches(input).Cast<Match>().Select(m => m.Value).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                sb.Append(string.Concat(Enumerable.Repeat(chars[i].ToUpper(), int.Parse(nums[i]))));
+            }
+            Console.WriteLine($"Unique symbols used: {CountUniqueChars(chars, nums)}");
+            Console.WriteLine(sb);
+        }
+
+        static int CountUniqueChars(string[] charsArr, string[] numsArr)
+        {
+            List<char> chars = new List<char>();
+            for (int i = 0; i < charsArr.Length; i++)
+            {
+                if (int.Parse(numsArr[i]) > 0)
+                {
+                    char[] symbols = charsArr[i].ToCharArray();
+                    foreach (char c in symbols)
+                    {
+                        char addChar = char.ToLower(c);
+                        if (!chars.Contains(addChar))
+                        {
+                            chars.Add(addChar);
+                        }
+                    }
+                }
+            }
+            return chars.Count();
+        }
+    }
+}
---
+++ MoreExercises/ME04.SantasSecretHelper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ME04.SantasSecretHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex match = new Regex(@"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<behaviour>[NG])!");
+            int decryptionKey = int.Parse(Console.ReadLine());
+
+            string input;
+            StringBuilder goodChildren = new StringBuilder();
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string decrypted = DecryptString(input, decryptionKey);
+                Match kid = match.Match(decrypted);
+                if (kid.Groups["behaviour"].Value.ToString() == "G")
+                {
+                    goodChildren.AppendLine(kid.Groups["name"].Value);
+                }
+            }
+
+            Console.WriteLine(goodChildren);
+        }
+
+        static string DecryptString(string s, int key)
+        {
+            StringBuilder decryptedString = new StringBuilder();
+            foreach (char c in s)
+            {
+                decryptedString.Append((char)(c - key));
+            }
+            return decryptedString.ToString();
+        }
+    }
+}
