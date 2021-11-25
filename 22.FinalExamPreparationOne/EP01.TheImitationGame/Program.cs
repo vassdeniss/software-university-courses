c20e9eb641f256b19698ab1a5bf697bd70229d16
Denis Vasilev <vassdeniss@gmail.com>
2021-11-25 10:28:32 +0200

    Forgot to stage files *facepalm*

---
+++ 22.FinalExamPreparationOne/EP01.TheImitationGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace EP01.TheImitationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+            string decryptedMessage = encryptedMessage;
+
+            string input = Console.ReadLine();
+            while (input != "Decode")
+            {
+                string[] cmd = input.Split('|');
+
+                if (cmd[0] == "Move")
+                    decryptedMessage = decryptedMessage[int.Parse(cmd[1])..] + decryptedMessage[..int.Parse(cmd[1])];
+                else if (cmd[0] == "Insert")
+                    decryptedMessage = decryptedMessage.Insert(int.Parse(cmd[1]), cmd[2]);
+                else if (cmd[0] == "ChangeAll")
+                    decryptedMessage = decryptedMessage.Replace(cmd[1], cmd[2]);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {decryptedMessage}");
+        }
+    }
+}
---
+++ 22.FinalExamPreparationOne/EP02.DestinationMapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EP02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(?<symbol>[=\/])(?<name>[A-Z][A-Za-z]{2,})\k<symbol>");
+            MatchCollection matches = pattern.Matches(Console.ReadLine());
+
+            int travelPoints = 0;
+            foreach (Match match in matches)
+                travelPoints += match.Groups["name"].Value.Length;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Destinations: ");
+            for (int i = 0; i < matches.Count; i++)
+            {
+                sb.Append(matches[i].Groups["name"].Value);
+                if (i != matches.Count - 1) sb.Append(", ");
+            }
+
+            sb.Append($"\nTravel Points: {travelPoints}");
+            Console.WriteLine(sb);
+        }
+    }
+}
