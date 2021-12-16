cae977248223bc57076e0398024e6946575d3e2a
Denis Vasilev <vassdeniss@gmail.com>
2021-12-16 12:07:30 +0200

    Committed everything up to and including: Combinatorial Problems - Lab

---
+++ 02.CombinatorialProblems/L02.PermutationsRepetitions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L02.PermutationsRepetitions
+{
+    internal class Program
+    {
+        private static string[] Permutations;
+
+        static void Main(string[] args)
+        {
+            Permutations = Console.ReadLine().Split();
+            Permutate(0);
+        }
+
+        private static void Permutate(int index)
+        {
+            if (index >= Permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", Permutations));
+                return;
+            }
+
+            Permutate(index + 1);
+            HashSet<string> used = new HashSet<string>() { Permutations[index] };
+
+            for (int i = index + 1; i < Permutations.Length; i++)
+            {
+                if (!used.Contains(Permutations[i]))
+                {
+                    Swap(index, i);
+                    Permutate(index + 1);
+                    Swap(index, i);
+                    used.Add(Permutations[i]);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second) =>
+            (Permutations[first], Permutations[second]) = (Permutations[second], Permutations[first]);
+    }
+}
---
+++ 02.CombinatorialProblems/L05.CombinationNoRepetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L05.CombinationNoRepetition
+{
+    internal class Program
+    {
+        private static int K;
+        private static string[] Input;
+        private static string[] Combinations;
+
+        static void Main(string[] args)
+        {
+            Input = Console.ReadLine().Split();
+            K = int.Parse(Console.ReadLine());
+            Combinations = new string[K];
+
+            Combinate(0, 0);
+        }
+
+        private static void Combinate(int index, int start)
+        {
+            if (index >= Combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", Combinations));
+                return;
+            }
+
+            for (int i = start; i < Input.Length; i++)
+            {
+                Combinations[index] = Input[i];
+                Combinate(index + 1, i + 1);
+            }
+        }
+    }
+}
