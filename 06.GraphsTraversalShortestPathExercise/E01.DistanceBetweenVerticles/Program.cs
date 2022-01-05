b4b3971d15decacf10d56b2f73f3e0e72dce033a
Denis Vasilev <vassdeniss@gmail.com>
2022-01-05 10:53:35 +0200

    Committed everything up to and including: Graph Theory, Traversal, Shortest Path - Exercise

---
+++ 06.GraphsTraversalShortestPathExercise/E01.DistanceBetweenVerticles/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E01.DistanceBetweenVerticles
+{
+    internal class Program
+    {
+        private static Dictionary<int, List<int>> Graph;
+        private static Dictionary<int, bool> Visited;
+        private static Dictionary<int, int> Parent;
+
+        static void Main(string[] args)
+        {
+            int verticlesQty = int.Parse(Console.ReadLine()); 
+            int pairsQty = int.Parse(Console.ReadLine());
+
+            Graph = RegisterGraph(verticlesQty);
+            for (int i = 0; i < pairsQty; i++)
+            {
+                Visited = RegisterVisitedList();
+                Parent = RegisterParentsList();
+
+                string[] data = Console.ReadLine().Split('-');
+                int start = int.Parse(data[0]);
+                int end = int.Parse(data[1]);
+                BFS(start, end);
+            }
+        }
+
+        private static Dictionary<int, List<int>> RegisterGraph(int n)
+        {
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(':', StringSplitOptions.RemoveEmptyEntries);
+                if (data.Length == 1)
+                {
+                    graph.Add(int.Parse(data[0]), new List<int>());
+                    continue;
+                }
+
+                List<int> children = data[1].Split().Select(int.Parse).ToList();
+                graph.Add(int.Parse(data[0]), children);
+            }
+
+            return graph;
+        }
+
+        private static Dictionary<int, bool> RegisterVisitedList()
+        {
+            Dictionary<int, bool> list = new Dictionary<int, bool>();
+            foreach (int n in Graph.Keys) list.Add(n, false);
+            return list;
+        }
+
+        private static Dictionary<int, int> RegisterParentsList()
+        {
+            Dictionary<int, int> list = new Dictionary<int, int>();
+            foreach (int n in Graph.Keys) list.Add(n, -1);
+            return list;
+        }
+
+        private static void BFS(int start, int target)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            Visited[start] = true;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue(); 
+
+                if (node == target)
+                {
+                    Stack<int> path = GetPath(target);
+                    Console.WriteLine($"{{{start}, {target}}} -> {path.Count - 1}");
+                    return;
+                }
+
+                foreach (int child in Graph[node])
+                {
+                    if (!Visited[child])
+                    {
+                        Visited[child] = true;
+                        queue.Enqueue(child);
+                        Parent[child] = node;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{{{start}, {target}}} -> -1");
+        }
+
+        private static Stack<int> GetPath(int target)
+        {
+            Stack<int> path = new Stack<int>();
+
+            int idx = target;
+            while (idx != -1)
+            {
+                path.Push(idx);
+                idx = Parent[idx];
+            }
+
+            return path;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Repo of solutions for every problem in Software University's "Algorithm Fundamen
 * [03. Recursion, Combinatorial Algorithms - Exercise - 16/12/2021](https://github.com/vassdeniss/softuni-algorithm-fundamentals-cs-december-2021-solutions/tree/master/03.RecursionCombinatorialAlgsExercise)
 * [04. Searching, Sorting, Greedy Algorithms - Lab - 23/12/2021](https://github.com/vassdeniss/softuni-algorithm-fundamentals-cs-december-2021-solutions/tree/master/04.SearchingSortingGreedyAlgorithms)
 * [05. Graph Theory, Traversal, Shortest Path - Lab - 30/12/2021](https://github.com/vassdeniss/softuni-algorithm-fundamentals-cs-december-2021-solutions/tree/master/05.GraphsTraversalShortestPath)
+* [06. Graph Theory, Traversal, Shortest Path - Exercise - 04/01/2022](https://github.com/vassdeniss/softuni-algorithm-fundamentals-cs-december-2021-solutions/tree/master/06.GraphsTraversalShortestPathExercise)
 
 ## :heart: Support
 Please make sure to :star: this repository to show your support <3
